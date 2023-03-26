c2495df0a6095bc105552724574ffa895806eb3c
chelm <chelmicki.p@gmail.com>
2023-03-26 16:32:08 +0200

    add extra board

---
+++ src/components/ExtraBoard.test.tsx
@@ -0,0 +1,34 @@
+import { afterEach, describe, expect, test } from 'vitest';
+import { ExtraBoard } from './ExtraBoard';
+import { renderWithProviders } from '../helopers/testHelpers';
+import { cleanup, fireEvent } from '@testing-library/react';
+
+describe('ExtraBoard', function () {
+
+    afterEach(() => {
+        cleanup()
+    });
+
+    describe('render', function () {
+        test('should render all buttons', function () {
+            const { getByText } = renderWithProviders(<ExtraBoard />);
+
+            expect(getByText('(')).exist;
+            expect(getByText(')')).exist;
+            expect(getByText('C')).exist;
+        });
+    });
+
+    describe('custom "C" handle', function () {
+        test('should clear expression value in store', function () {
+            const initState = { expression: { current: '123+32' } } as any;
+            const { store, getByText } = renderWithProviders(<ExtraBoard />, {
+                initState
+            });
+
+            fireEvent.click(getByText('C'));
+
+            expect(store.getState().expression.current).toEqual('');
+        });
+    });
+});
