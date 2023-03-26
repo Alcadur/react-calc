bb09a044745d8c3420ffaec05aaaa69ef4f30335
chelm <chelmicki.p@gmail.com>
2023-03-26 16:15:36 +0200

    add empty Calc component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Calc } from './components/Calc';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
@@ -5,7 +6,7 @@ function App() {
     return (
         <Provider store={store}>
             <div className="App">
-
+                <Calc/>
             </div>
         </Provider>
     );
