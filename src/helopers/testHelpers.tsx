9e0256b2eaa0d330d8a0100c049ebd40efd86385
chelm <chelmicki.p@gmail.com>
2023-03-26 16:25:51 +0200

    add renderWithProviders test helper

---
+++ src/helopers/testHelpers.tsx
@@ -0,0 +1,25 @@
+import { PropsWithChildren, ReactElement } from 'react';
+import { PreloadedState } from '@reduxjs/toolkit';
+import { render, RenderOptions } from '@testing-library/react';
+import { AppStore, RootState, setupStore } from '../redux/store';
+import { Provider } from 'react-redux';
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+    initState?: PreloadedState<RootState>;
+    store?: AppStore;
+}
+
+export function renderWithProviders(
+    ui: ReactElement,
+    {
+        initState = {},
+        store = setupStore(initState),
+        ...renderOptions
+    }: ExtendedRenderOptions = {}
+) {
+    function Wrapper({ children }: PropsWithChildren<Object>): JSX.Element {
+        return <Provider store={store}>{children}</Provider>;
+    }
+
+    return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
